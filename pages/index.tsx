8ee5e5125761ed1e9c7b820b421c4ab89d3ced54
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-03-11 12:41:21 -0300

    Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Jourly</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        aaaa
+      </main>
+    </div>
+  )
+}
